061ecb09b190388e6a44fb69f4631e9c485fb1b9
Hassan Haroon <48328091+hassanharoon321@users.noreply.github.com>
2022-12-11 16:00:22 +0000

    main

--- index.js
+++ index.js
@@ -4,11 +4,27 @@
 //Math => 1
 //Arrays => 4
 
-const date = new Date();
 
+function sortArrays(arr)
+{
+ 
+        let length = arr.length;
+   
+        for (let j = 0; j < length - 1; j++) {
+            
+            if (arr[j] > arr[j + 1]) {
+   
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                j = -1;
+            }
+        }
+   
+        return arr;
+}
 
+let arr=[1, 2, 99, 9, 8,7, 6, 0, 5, 4, 3];
 
-
-
-console.log(formatDate(date));
-
+let ans = sortArrays(arr);
+console.log(ans)
