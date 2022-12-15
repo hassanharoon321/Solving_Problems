59f495ed526bc6446e575e031ec46e76d49028d8
Hassan Haroon <48328091+hassanharoon321@users.noreply.github.com>
2022-12-15 16:51:43 +0000

    morse code

---
+++ Strings/MorseCode.js
@@ -0,0 +1,7 @@
+const alphabet = {
+  a: '.-', b: '-...', c: '-.-.', d: '-..', e: '.', f: '..-.', g: '--.', h: '....', i: '..', j: '.---', k: '-.-', l: '.-..', m: '--',
+  n: '-.', o: '---', p: '.--.', q: '--.-', r: '.-.', s: '...', t: '-', u: '..-', v: '...-', w: '.--', x: '-..-', y: '-.--', z: '--..'
+}
+var uniqueMorseRepresentations = function(words) {
+  return new Set(words.map(word => word.split('').map(letter => alphabet[letter]).join(''))).size
+}
